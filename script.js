831a6d0c20f5928285c1e51f08bbbe6c699009e5
KarachunVladislav01 <karachunvladislav@gmail.com>
2020-02-28 13:07:26 +0300

    feat: added adding movies with verification

--- script.js
+++ script.js
@@ -44,39 +44,106 @@ webix.ui({
 
 const form = {
   view: "form",
+  id: "addFilmForm",
   minWidth: 150,
   maxWidth: 250,
   elements: [
     { template: "EDIT FILMS", type: "section" },
     {
       view: "text",
-      label: "Title"
+      label: "Title",
+      name: "title",
+      invalidMessage: "Must be filled"
     },
     {
       view: "text",
-      label: "Year"
+      label: "Year",
+      name: "year",
+      invalidMessage: "1970 - the current year"
     },
     {
       view: "text",
-      label: "Rating"
+      label: "Rating",
+      name: "rating",
+      invalidMessage: "Cannot be zero"
     },
     {
       view: "text",
-      label: "Votes"
+      label: "Votes",
+      name: "votes",
+      invalidMessage: "must be > 100000"
     },
     {
       margin: 20,
       cols: [
-        { view: "button", label: "Add new", css: "webix_primary" },
-        { view: "button", label: "Clear" }
+        {
+          view: "button",
+          label: "Add new",
+          css: "webix_primary",
+          click: () => {
+            const form = $$("addFilmForm");
+            const isValid = form.validate();
+            if (isValid) {
+              const formData = form.getValues();
+              $$("filmsDataTable").add(formData);
+              webix.message({
+                text: "Adding was successful",
+                type: "success",
+                expire: 2000
+              });
+              form.clear();
+            }
+            {
+              webix.message({
+                text: "Invalid data",
+                type: "error",
+                expire: 2000
+              });
+            }
+          }
+        },
+        {
+          view: "button",
+          label: "Clear",
+          click: () => {
+            confirmOfClearForm();
+          }
+        }
       ]
     },
     {}
-  ]
+  ],
+  rules: {
+    title: webix.rules.isNotEmpty,
+    year: value => {
+      const now = new Date();
+      return value > 1970 && value < now.getFullYear();
+    },
+    votes: value => {
+      return value != "" && value < 100000;
+    },
+    rating: value => {
+      return value != "" && value != 0;
+    }
+  }
+};
+
+const confirmOfClearForm = () => {
+  webix
+    .confirm({
+      title: "Clear form",
+      text: "Are you sure?"
+    })
+    .then(() => {
+      const form = $$("addFilmForm");
+      form.clear();
+      form.clearValidation();
+    });
 };
 
 const dataTable = {
   view: "datatable",
+  id: "filmsDataTable",
   minWidth: 540,
   data: filmsSet,
   scrollX: false,
